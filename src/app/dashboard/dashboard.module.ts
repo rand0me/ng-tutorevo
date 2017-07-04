aed480d7c5edf694ef0eda633bcb7525850ea51a
Mark Bober <beavermark93@gmail.com>
2017-07-05 00:57:20 +0300

    Added envs, changed favicon, styles of chat comp

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,8 +2,10 @@ import { NgModule }       from '@angular/core';
 import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
+import { environment } from "../../environments/environment";
+
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const socketIoConfig: SocketIoConfig = { url: 'http://192.168.5.46:3000', options: {} };
+const socketIoConfig: SocketIoConfig = { url: environment.socketUrl, options: {} };
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ChatComponent }          from './chat/chat.component';
